89bb511ced69c3a78da2c38ad902182ec849453c
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-17 23:57:24 +0900

    タイトルのurlに応じた表示

---
+++ src/app/blog/entry/[title]/page.tsx
@@ -0,0 +1,33 @@
+import { notFound } from "next/navigation";
+interface Article {
+    title: string;
+    content: string;
+    date: string;
+}
+
+const getArticle = async (slug: string) => {
+    const res = await fetch(`http://localhost:8080/blog/${slug}`);
+    if (res.status === 404) {
+        notFound();
+    }
+    if (!res.ok) {
+        throw new Error("Failed to fetch article");
+    }
+    const data = await res.json();
+
+    return data as Article;
+}
+
+export default async function Article({ params }: { params: { title: string } }) {
+    const articlePromise = getArticle(params.title);
+    const article = await articlePromise;
+
+    return (
+        <div>
+            <h1>{article.title}</h1>
+            <h2>{article.date}</h2>
+            <p>{article.content}</p>
+        </div>
+    );
+};
+
