134dd55c34944f47165755c8456a3c98ed744601
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-29 13:56:32 +0900

    プロジェクト

---
+++ src/app/project/page.tsx
@@ -0,0 +1,25 @@
+import styles from "@/app/css/project.module.css"
+import Header from "@/app/components/Header"
+import projectList from './projectList.json'
+export default function Project() {
+    return (
+        <>
+            <Header />
+            <div className={styles.page}>
+                <h1 className={styles.title}>プロジェクト</h1>
+                <ul className={styles.project}>
+                    {projectList.map((project) => (
+                        <li className={styles.item}>
+                            <h2>{project.projectName}</h2>
+                            <p>技術スタック: {project.techStack.join(', ')}</p>
+                            <p>担当: {project.field}</p>
+                            <p> {project.summary}</p>
+                            {project.link && <a href={project.link}>Link</a>}
+                            <p>{project.date}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+}
