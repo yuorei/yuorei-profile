44a871f75014c7d43401b18ba0c32a7ed0e7bcb3
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-29 18:10:14 +0900

    使えないためコメントアウト

--- src/app/blog/entry/[title]/page.tsx
+++ src/app/blog/entry/[title]/page.tsx
@@ -1,35 +1,35 @@
-import { notFound } from "next/navigation";
-import ReactMarkdown from "react-markdown";
-interface Article {
-    title: string;
-    content: string;
-    date: string;
-}
+// import { notFound } from "next/navigation";
+// import ReactMarkdown from "react-markdown";
+// interface Article {
+//     title: string;
+//     content: string;
+//     date: string;
+// }
 
-const getArticle = async (slug: string) => {
-    const res = await fetch(`http://localhost:8080/blog/${slug}`);
-    if (res.status === 404) {
-        notFound();
-    }
-    if (!res.ok) {
-        throw new Error("Failed to fetch article");
-    }
-    const data = await res.json();
+// const getArticle = async (slug: string) => {
+//     const res = await fetch(`http://localhost:8080/blog/${slug}`);
+//     if (res.status === 404) {
+//         notFound();
+//     }
+//     if (!res.ok) {
+//         throw new Error("Failed to fetch article");
+//     }
+//     const data = await res.json();
 
-    return data as Article;
-}
+//     return data as Article;
+// }
 
-export default async function Article({ params }: { params: { title: string } }) {
-    const articlePromise = getArticle(params.title);
-    const article = await articlePromise;
+// export default async function Article({ params }: { params: { title: string } }) {
+//     const articlePromise = getArticle(params.title);
+//     const article = await articlePromise;
 
-    return (
-        <div>
-            <h1>{article.title}</h1>
-            <h2>{article.date}</h2>
-            <p>{article.content}</p>
-            <ReactMarkdown>{article.content}</ReactMarkdown>;
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h1>{article.title}</h1>
+//             <h2>{article.date}</h2>
+//             <p>{article.content}</p>
+//             <ReactMarkdown>{article.content}</ReactMarkdown>;
+//         </div>
+//     );
+// };
 
