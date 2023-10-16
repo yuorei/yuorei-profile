a104a44fa5d2109597b3b7b9c3986bfd075c4bd9
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-16 14:23:32 +0900

    ヘッダーをスマホにも対応

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,26 +1,121 @@
-import styles from './Header.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-const Header: React.FC<{}> = () => {
-    return (
-        <div>
-            <header className={styles.header}>
-                <Link href="/">
-                    <Image src="/home/logo.png" alt="yuorei logo" width={300} height={100} />
-                </Link>
-                <nav >
-                    <ul className={styles.menu}>
-                        <li ><a href="/#yuorei">ユオレイ</a></li>
-                        <li ><a href="/skill">スキル</a></li>
-                        <li ><a href="/project">プロジェクト</a></li>
-                        <li ><a href="/history">歴史</a></li>
-                        <li ><a href="/hoby">趣味</a></li>
-                        <li ><a href="/picture">写真</a></li>
-                        <li ><a href="/#sns">SNS</a></li>
-                    </ul>
-                </nav>
-            </header>
+'use client';
+import styles from "./Header.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import React, {useState} from "react"
+export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  }
+  return (
+    <React.Fragment>
+      <header id="header" className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/">
+            {/* <div className={styles.logo}></div> */}
+            <Image src="/home/logo.png" alt="yuorei" width={200} height={100} />
+          </Link>
         </div>
-    )
+        <nav>
+          <ul>
+            <li>
+              <Link href="/skill">
+              <div>スキル</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="/history">
+              <div>経歴</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+              <div>ブログ</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="/project">
+              <div>プロジェクト</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="/hoby">
+              <div>趣味</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <div>ホーム</div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.container}>
+          <div className={styles.humburger} onClick={() => menuFunction()}>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <p className={openMenu ? styles.open : undefined}>Menu</p>
+          </div>
+        </div>
+      </header>
+      <div className={`${styles.drawerMenu} ${openMenu ? styles.open : undefined}`}>
+        <ul>
+          <div className={styles.close} onClick={() => menuFunction()}>
+            <span></span>
+            <span></span>
+            <p>Close</p>
+          </div>
+          <li>
+            <Link href="/">
+              <div>
+                <p className={styles.mainTitle}>ホーム</p>
+                {/* <p className={styles.subTitle}>ユオレイ</p> */}
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/skill">
+            <div>
+              <p className={styles.mainTitle}>スキル</p>
+              {/* <p className={styles.subTitle}></p> */}
+            </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/history">
+            <div>
+              <p className={styles.mainTitle}>経歴</p>
+              {/* <p className={styles.subTitle}></p> */}
+            </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+            <div>
+              <p className={styles.mainTitle}>ブログ</p>
+              {/* <p className={styles.subTitle}></p> */}
+            </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/project">
+            <div>
+              <p className={styles.mainTitle}>プロジェクト</p>
+              {/* <p className={styles.subTitle}></p> */}
+            </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/hoby">
+            <div>
+              <p className={styles.mainTitle}>趣味</p>
+              {/* <p className={styles.subTitle}></p> */}
+            </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
 }
-export default Header
