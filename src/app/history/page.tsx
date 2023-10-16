2b5f1dcbb0421ce24a961ea8dfb85a55a131e2c1
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-16 14:33:45 +0900

    fix

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,20 +4,22 @@ import Link from 'next/link'
 import historyList from './historyList.json'
 export default function History() {
     return (
-        <div className={styles.history}>
+        <>
             <Header />
-            <h1 className={styles.title}>経歴</h1>
-            <ul className={styles.timeline}>
-                {historyList.map((history, index) => (
-                    <li className={styles.item} key={index}>
-                        <h3>{history.title}</h3>
-                        <h4>{history.status}</h4>
-                        <Link href={history.link} className={styles.link}>{history.linkTitle} </Link>
-                        <p>{history.description}</p>
-                        <p className={styles.date}>{history.date}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+            <div className={styles.history}>
+                <h1 className={styles.title}>経歴</h1>
+                <ul className={styles.timeline}>
+                    {historyList.map((history, index) => (
+                        <li className={styles.item} key={index}>
+                            <h3>{history.title}</h3>
+                            <h4>{history.status}</h4>
+                            <Link href={history.link} className={styles.link}>{history.linkTitle} </Link>
+                            <p>{history.description}</p>
+                            <p className={styles.date}>{history.date}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 }
