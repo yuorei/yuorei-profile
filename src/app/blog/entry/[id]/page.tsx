4663f2907c413ca0c3a43b780ee599d467199c8f
yuorei <108039575+yuorei@users.noreply.github.com>
2024-05-07 23:20:29 +0900

    blog.yuorei.comにリダイレクト

--- src/app/blog/entry/[id]/page.tsx
+++ src/app/blog/entry/[id]/page.tsx
@@ -1,42 +1,7 @@
-'use client'
-import ReactMarkdown from "react-markdown";
-import styles from "@/app/css/article.module.css"
-import Header from "@/app/components/Header"
-import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Article } from '../../page';
+"use client";
+import { redirect } from "next/navigation";
 
 export default function Article({ params }: { params: { id: string } }) {
-    const [article, setArticle] = useState<Article>();
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog/${params.id}`);
-                if (response.status === 404) {
-                    notFound();
-                }
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-                const data = await response.json();
-                setArticle(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, [params.id]);
-    return (
-        <>
-            <Header />
-            <div className={styles.page}>
-                <h1>{article?.Title}</h1>
-                <h2>{article?.Date}</h2>
-                <div className={styles.blog}>
-                    <img src={article?.OGPURL as string} alt={article?.Title as string} style={{ width: '60%', height: 'auto', objectFit: 'contain' }} />
-                    <ReactMarkdown className={styles.md}>{article?.Content}</ReactMarkdown>
-                </div>
-            </div >
-        </>
-    );
-};
+  console.log(`redirect to https://blog.yuorei.com/entry/${params.id}`);
+  redirect(`https://blog.yuorei.com/entry/${params.id}`);
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,64 +1,7 @@
-'use client';
-import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
-import Link from 'next/link';
-import Header from "@/app/components/Header"
-import styles from "@/app/css/blog.module.css"
-
-export type Article = {
-    ID: string;
-    Title: string;
-    Content: string;
-    Date: string;
-    IsPublic: boolean;
-    OGPURL: string;
-};
+"use client";
+import { redirect } from "next/navigation";
 
 export default function Blog() {
-    // const articles = await getArticles();
-    const [articles, setArticles] = useState<Article[]>([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog`);
-                if (response.status === 404) {
-                    notFound();
-                }
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-                const data: Article[] = await response.json();
-                // Dateを降順でソート
-
-                const sortedData = data.sort((a, b) => new Date(b.Date).getTime() - new Date(a.Date).getTime());
-
-                setArticles(sortedData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    return (
-        <>
-            <Header />
-            <div className={styles.blog}>
-                <h1>yuorei blog</h1>
-                <ul>
-                    {articles.map((article, index) => (
-                        <Link href={`blog/entry/${article.ID}`} key={index}>
-                            <li className={styles.list} >
-                                <div>
-                                    <h2>{article.Title}</h2>
-                                    <p>{article.Date}</p>
-                                </div>
-                                <img src={article.OGPURL} alt={article.Title} style={{ width: '10%', height: 'auto', objectFit: 'contain' }} />
-                            </li>
-                        </Link>
-                    ))}
-                </ul>
-            </div>
-        </>
-    );
+  console.log("redirect to https://blog.yuorei.com/");
+  redirect(`https://blog.yuorei.com/`);
 }
