6738fc387058ba61e0c61c26aced11448cc1e8bb
yuorei <108039575+yuorei@users.noreply.github.com>
2024-05-10 15:07:43 +0900

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,11 @@
-import Header from "@/app/components/Header"
-import styles from "@/app/css/not-found.module.css"
+import styles from "@/app/css/not-found.module.css";
 export default async function NotFound() {
-    return (
-        <>
-        <Header />
-        <div className={styles.not_found}>
-            <h1 >404  </h1>
-            <p>ページが見つかりませんでした</p>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.not_found}>
+        <h1>404 </h1>
+        <p>ページが見つかりませんでした</p>
+      </div>
+    </>
+  );
 }
